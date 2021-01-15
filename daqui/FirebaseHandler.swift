96205f7f6564180e786e6bd7a112a8af1672d33b
Samuel <samuelbsantosn@gmail.com>
2021-01-15 20:50:11 -0300

    Criando firebase

---
+++ daqui/FirebaseHandler.swift
@@ -0,0 +1,183 @@
+//
+//  FirebaseManager.swift
+//  daqui
+//
+//  Created by Samuel Brasileiro on 15/01/21.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseStorage
+
+enum Collection: String, Codable {
+    case costumers
+    case businesses
+    case trails
+    case missions
+    case categories
+    case prizes
+    
+    
+    //resolve esse problema
+    let folder {
+        get{
+            return self + "/"
+        }
+    }
+}
+
+class FirebaseHandler {
+    
+    static var ref = Database.database().reference()
+    
+    static var storage = Storage.storage().reference()
+    
+    init() {
+        
+    }
+    
+    
+    
+    /// Adding an object to a collection
+    /// - Parameters:
+    ///   - collection: Collections are the categories in which the objects are stored
+    ///   - value: The value of the objects
+    class func writeToCollection<Type>(_ collection: Collection, value: Type)
+    where Type: Codable{
+        let childRef = ref.child(collection.rawValue).childByAutoId()
+        
+        do {
+            var dictionary = try value.asDictionary()
+            dictionary["id"] = childRef.key
+            
+            childRef.setValue(dictionary)
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    class func getItemImage<Type>(_ collection: Collection, from id: String, completion: @escaping (Result<UIImage, Error>) -> Void){
+        let ref = storage.child(collection.folder)
+        
+        let imageRef = ref.child(id)
+        
+        imageRef.getData(maxSize: 1 * 2048 * 2048){ data, error in
+            
+            if let error = error{
+                print(error.localizedDescription)
+                completion(.failure(error))
+            }
+            if let data = data{
+                if let image = UIImage(data: data){
+                    completion(.success(image))
+                }
+            }
+        }
+    }
+    
+    
+    /// Reading the objects written in the previous operation
+    /// - Parameters:
+    ///   - collection: Collections are the categories in which the objects are stored
+    ///   - id: Is an identifier to the object. Each object has a unique id
+    ///   - dataType: Identifying the type of the collection where the objects are stored
+    ///   - completion: When the reading is over, this code is executed, it can be either successful or a failure
+    class func readCollection<Type>(_ collection: Collection, id: String, dataType: Type.Type, completion: @escaping (Result<Type,Error>) -> Void)
+    where Type: Codable{
+        
+        let pathReference = ref.child(collection.rawValue + id)
+        
+        pathReference.observeSingleEvent(of: .value, with: {(snapshot) in
+            DispatchQueue.main.async {
+                
+                guard var data = snapshot.valueInExportFormat() as? [String: Any] else{
+                    return
+                }
+                
+                //Convert Dictionary Items to Array
+                var dict = data
+                for key in data.keys{
+                    if let data = data[key] as? [String: Any]{
+                        dict[key] = data.map{$0.value}
+                        
+                    }
+                }
+                data = dict
+                do{
+                    
+                    let decodedData = try Type(from: data)
+                    completion(.success(decodedData))
+                }
+                catch{
+                    completion(.failure(error))
+                }
+            }
+            
+        })
+    }
+    
+    
+    /// Reading all the collections at the same time
+    /// - Parameters:
+    ///   - collection: Collections are the categories in which the objects are stored
+    ///   - dataType: Identifying the type of the collection where the objects are stored
+    ///   - completion: When the reading is over, this code is executed, it can be either successful or a failure
+    class func readAllCollection<Type>(_ collection: Collection, dataType: Type.Type, completion: @escaping (Result<Type,Error>) -> Void)
+    where Type: Codable{
+        
+        let collectionRef = ref.child(collection.rawValue)
+        
+        collectionRef.observeSingleEvent(of: .value, with: {(snapshot) in
+            
+            DispatchQueue.main.async {
+                
+                guard let dict = snapshot.valueInExportFormat() as? [String: Any] else{
+                    return
+                }
+                
+                //Convert Dictionary to Array (uma gambiarra)
+                var data = dict.map{$0.value}
+                data = data.map{ item -> Any? in
+                    if var dict = item as? [String: Any]{
+                        for key in dict.keys{
+                            if let data = dict[key] as? [String: Any]{
+                                dict[key] = data.map{$0.value}
+                                return dict
+                            }
+                        }
+                    }
+                    return item
+                }
+                
+                do{
+                    let thing = try Type(from: data)
+                    completion(.success(thing))
+                }
+                catch{
+                    print(error)
+                    completion(.failure(error))
+                }
+            }
+            
+        })
+    }
+    
+}
+extension Decodable {
+    init(from data: Any) throws {
+        let data = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
+        
+        self = try JSONDecoder().decode(Self.self, from: data)
+    }
+}
+
+extension Encodable {
+    func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError()
+        }
+        return dictionary
+    }
+}
