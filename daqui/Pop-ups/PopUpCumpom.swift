1116d3b2b45e3d24f26b31765bf8d1a0e61dc5db
Alanis <alanislimasantaclara00@gmail.com>
2021-01-16 10:47:17 -0300

    tela popup do premio

---
+++ daqui/Pop-ups/PopUpCumpom.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpCumpom.swift
+//  daqui
+//
+//  Created by Alanis Lima Santa Clara on 16/01/21.
+//
+
+import SwiftUI
+
+struct PopUpCumpom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopUpCumpom_Previews: PreviewProvider {
+    static var previews: some View {
+        PopUpCumpom()
+    }
+}
