321c74f8e43e9de25e60c726a088a258fdf6fbd8
Samuel <samuelbsantosn@gmail.com>
2021-01-17 12:35:58 -0300

    Criando missoes estaticas

--- daqui/Models/Mission.swift
+++ daqui/Models/Mission.swift
@@ -11,12 +11,11 @@ import UIKit
 class Mission: ObservableObject {
     
     var attributes: Mission.Database
-    @Published var category: Category?
     @Published var categoryImage: UIImage?
     
     init(attributes: Mission.Database) {
         self.attributes = attributes
-        self.getCategory()
+        self.getImage(url: self.attributes.imgURL)
     }
     
     func getImage(url: String){
@@ -33,29 +32,29 @@ class Mission: ObservableObject {
         } .resume()
     }
     
-    func getCategory() {
-        FirebaseHandler.readCollection(.categories, id: self.attributes.idCategory, dataType: Category.Database.self, completion: { result in
-            if case .success(let category) = result{
-                self.category = Category(attributes: category)
-                self.getImage(url: self.category!.attributes.iconURL)
-            }
-        })
-    }
-    
     class Database: Codable {
         var title: String
         var description: String
         var isCompleted: Bool
         var id: String
-        var idCategory: String
+        var imgURL: String
         
-        init(title: String, description: String, isCompleted: Bool, id: String, idCategory: String) {
+        init(title: String, description: String, isCompleted: Bool, id: String, imgURL: String) {
             self.title = title
             self.description = description
             self.isCompleted = isCompleted
             self.id = id
-            self.idCategory = idCategory
+            self.imgURL = imgURL
         }
     }
+    
+    static var missions: [Mission] = [
+        Mission.Database(title: "Divulgação no Whatsapp", description: "O cliente irá compartilhar informações do seu negócio com contatos de seu whatsapp", isCompleted: false, id: "", imgURL: "https://cdn.discordapp.com/attachments/788903633268768839/800383822218526760/Divulgacao.png"),
+        Mission.Database(title: "Compra antecipada", description: "Aqui você vai gerar capital de giro para seu negócio, através da venda de cupons", isCompleted: false, id: "", imgURL: "https://cdn.discordapp.com/attachments/788903633268768839/800383828296335420/Antecipada.png"),
+        Mission.Database(title: "Post nos Stories", description: "O cliente irá publicar um stories no instagram divulgando sua marca ou produto", isCompleted: false, id: "", imgURL: "https://cdn.discordapp.com/attachments/788903633268768839/800383826974474270/Publicidade.png"),
+        Mission.Database(title: "Pesquisa de mercado", description: "Através de uma enquete, você poderá testar tendências de mercado com o seu público", isCompleted: false, id: "", imgURL: "https://cdn.discordapp.com/attachments/788903633268768839/800368599755784212/MarkeIcon.png"),
+        Mission.Database(title: "Feedback do cliente", description: "Aqui você disponibiliza uma caixa de texto para o cliente compartilhar a sua experiência ou opinião!", isCompleted: false, id: "", imgURL: "https://cdn.discordapp.com/attachments/788903633268768839/800383824822796298/Feedback.png"),
+        Mission.Database(title: "Publicação no Facebook", description: "O cliente irá compartilhar a página do seu negócio no Facebook", isCompleted: false, id: "", imgURL: "https://discord.com/channels/788903632824959026/788903633268768839/800386517994176573")
+    ].map{Mission(attributes: $0)}
     
 }
